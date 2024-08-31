78c9c0dba0916eaab5d562c11428d01687d329b7
DhruvMaradiya <76841849+DhruvMaradiya@users.noreply.github.com>
2024-08-31 16:39:54 -0400

    Add files via upload

---
+++ weather-x-node-project/config.js
@@ -0,0 +1,8 @@
+const constants = {
+    openWeatherMap: {
+        BASE_URL: "https://api.openweathermap.org/data/2.5/forecast?q=",
+        SECRET_KEY: "66662c8f99857d855a7803978ccceba3"
+    }
+}
+
+module.exports = constants;
